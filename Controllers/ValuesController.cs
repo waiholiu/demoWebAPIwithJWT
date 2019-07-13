4d23d214cacc4650ac36ff9e8a14a64b19c62388
waiholiu <waiholiu>
2019-07-13 15:41:23 +1000

    init

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testWebAPIFB.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IHttpContextAccessor _contextAccessor;
+        public ValuesController(IHttpContextAccessor contextAccessor)
+        {
+
+            _contextAccessor = contextAccessor;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var name = _contextAccessor.HttpContext.User.Claims.First(c => c.Type == "name").Value;
+            return new string[] { name, "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
